4e28d1adfacdcf261db933cf26c19fa3f3cccc8d
alfahadiraihan <raihanhadi100@gmail.com>
2025-07-05 21:39:03 +0700

    initial commit

---
+++ Config.cs
@@ -0,0 +1,9 @@
+// Global constants
+namespace RaylibDanmaku
+{
+    internal static class Config
+    {
+        public const int SCREEN_WIDTH = 640;
+        public const int SCREEN_HEIGHT = 960;
+    }
+}
